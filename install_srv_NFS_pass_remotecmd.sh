1e0407574718e349ee907e254db07033342c9f9d
gb-0001 <gb-0001@outlook.fr>
2021-04-25 19:31:06 +0200

    MAj NFS remote svc autofs pb emacs dailog window2

--- install_srv_NFS_pass_remotecmd.sh
+++ install_srv_NFS_pass_remotecmd.sh
@@ -34,7 +34,7 @@ FXAUTOFS_BACKUPCRON () {
     sudo apt -y update
     sudo export DEBIAN_FRONTEND=noninteractive
     sudo apt -y install nfs-common autofs
-    sudo mkdir -p $1/$2
+    sudo mkdir -p $1$2
     sudo sh -c "echo '$1    /etc/auto.nas --timeout 60' >> /etc/auto.master"
     sudo sh -c "echo '$2  -rw,soft,intr,rsize=8192,wsize=8192 $3:$4' >> /etc/auto.nas"
     sudo chmod 644 /etc/auto.nas
