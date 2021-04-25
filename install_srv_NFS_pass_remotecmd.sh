d4d5f74d0782a15fd78eb034320729cf5876b7d5
gb-0001 <gb-0001@outlook.fr>
2021-04-25 02:58:47 +0200

    MAJ NFS install addon

--- install_srv_NFS_pass_remotecmd.sh
+++ install_srv_NFS_pass_remotecmd.sh
@@ -22,26 +22,24 @@ DESTINATION_JENKINS_BACKUP=$BACKUPDIR$BACKUP_JENKINS_DIR
 
 #Fonction FXAUTOFS_BACKUPCRON Mise en place du montage automatique avec autofs sur le serveur et planification de la sauvegarde 1x par Heure et retention sur 7j
 #parametre fonction
-#$1 ==> exemple $LOGINUSER
-#$2 ==> exemple $SRVWEB
-#$3 ==> exemple $BACKUPDIR
-#$4 ==> exemple $BACKUP_WEB_DIR
-#$5 ==> exemple $SRVNFS
-#$6 ==> exemple $NFSWEB_MOUNTDIR
-#$7 ==> exemple $SOURCE_WEB_DIR
-#$8 ==> exemple $DESTINATION_WEB_BACKUP
-#$9 ==> exemple $BACKUPWEB_FILENAME
+#$1 ==> exemple $BACKUPDIR
+#$2 ==> exemple $BACKUP_WEB_DIR
+#$3 ==> exemple $SRVNFS
+#$4 ==> exemple $NFSWEB_MOUNTDIR
+#$5 ==> exemple $SOURCE_WEB_DIR
+#$6 ==> exemple $DESTINATION_WEB_BACKUP
+#$7 ==> exemple $BACKUPWEB_FILENAME
 
 FXAUTOFS_BACKUPCRON () {
-sudo apt -y install autofs
-sudo mkdir $3
-sudo echo "$3    /etc/auto.nas --timeout 60" >> /etc/auto.master
-echo "$4  -rw,soft,intr,rsize=8192,wsize=8192 $5:$6" >> /etc/auto.nas
-sudo chmod 644 /etc/auto.nas
-sudo service autofs reload
-YEAR=date +%Y && MONTH=date +%m && DAY=date +%d
-echo "* */1 * * * tar cvzfP $7 $8/$9_$DAY_$MONTH_$YEAR.tar.gz " >> /var/spool/cron/crontabs/vagrant
-echo "15 4 * * * find $8 -name "*.tar.gz" -type f -mtime +7 -exec rm -f {} +" >> /var/spool/cron/crontabs/vagrant
+    sudo apt -y install autofs
+    sudo mkdir $1
+    sudo echo "$1    /etc/auto.nas --timeout 60" >> /etc/auto.master
+    echo "$2  -rw,soft,intr,rsize=8192,wsize=8192 $3:$4" >> /etc/auto.nas
+        sudo chmod 644 /etc/auto.nas
+    sudo service autofs reload
+    YEAR=date +%Y && MONTH=date +%m && DAY=date +%d
+    echo "* */1 * * * tar cvzfP $5 $6/$7_$DAY_$MONTH_$YEAR.tar.gz " >> /var/spool/cron/crontabs/vagrant
+    echo "15 4 * * * find $6 -name "*.tar.gz" -type f -mtime +7 -exec rm -f {} +" >> /var/spool/cron/crontabs/vagrant
 }
 
 #Mise en place du montage automatique avec autofs sur le serveur web et planification de la sauvegarde 1x par Heure et retention sur 7j
