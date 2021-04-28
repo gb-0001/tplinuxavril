57ee3cc78095fa0c4e01832186f1a95c16efd285
gb-0001 <gb-0001@outlook.fr>
2021-04-28 17:14:25 +0200

    maj install_dev

--- install_dev.sh
+++ install_dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#script d'installation d'un poste devellopeur
+#script d'installation d'un poste developpeur
+
+#recuperation des sources nécessaires aux déploiement
+cd /tmp && wget -O corbeille_1.0.0.deb https://github.com/gb-0001/tplinuxavril/raw/master/sources/corbeille_1.0.0.deb
+cd /tmp && wget -O varenv.sh https://github.com/gb-0001/tplinuxavril/raw/master/sources/varenv.sh
+cd /tmp && wget -O install_dev.sh https://github.com/gb-0001/tplinuxavril/raw/master/install_dev.sh
 
 #Parametre
 SRVINTEGRATION=192.168.0.19
